9d4517e99c7ddf6af6ed15c9ee63f278accc87b2
Will Studabaker <will.studabaker@gmail.com>
2020-10-15 18:12:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code below!
+// write your code below
+function happyHolidays(){
+  return "Happy Holidays!"
+}
+function happyHolidaysTo(name){
+  return `Happy Holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, numDays){
+  return `It's ${numDays} days until ${holiday}!`
+}
